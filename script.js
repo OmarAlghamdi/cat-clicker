aabe89ceb41fc541a4f8717b9fc3ab02896b0811
Omar Alghamdi <mr.alghamdi.omar@gmail.com>
2019-11-29 19:23:08 +0300

    v3 counts do not persist when change the image

--- script.js
+++ script.js
@@ -22,23 +22,49 @@ const cats = [
 ]
 
 
-const container = document.querySelector('.container')
+const container = document.querySelector('.cats-container')
+const menu = document.querySelector('.menu')
+
 for (const cat in cats) {
     let counter = 0
-    const catElement = document.createElement('div')
-    catElement.className = 'cat-container'
-    catElement.innerHTML = 
-    `<h4 class="cat-name">${cats[cat].name}</h4>
-    <p><span id="${cat}">${counter}</span> clicks</p>
-    <img class='cat-img' src="${cats[cat].src}" />`
+    const catImg = document.createElement('div')
+    catImg.innerHTML =
+        `<h4 class="cat-name">${cats[cat].name}</h4>`
 
-    container.appendChild(catElement)
-    
-    catElement.addEventListener('click', ((counter, id) => {
+    menu.appendChild(catImg)
+
+    catImg.addEventListener('click', ((id, counter) => {
         return () => {
-            document.getElementById(id).innerHTML = ++counter
-        }
-    })(counter, cat))
+            oldCat = document.querySelector('.cat')
+            const newCat = document.createElement('div')
+            newCat.className = 'cat'
+            newCat.innerHTML =
+                `<h4 class="cat-name">${cats[id].name}</h4>
+                <p><span id="${id}">${counter}</span> clicks</p>
+                <img class='cat-img' src="${cats[id].src}" />`
+            container.replaceChild(newCat, oldCat)
 
+            newCat.addEventListener('click', ((counter, id) => {
+                return () => {
+                    document.getElementById(id).innerHTML = ++counter
+                }
+            })(counter, id))
+        }
+
+    })(cat, counter))
     
+    // catImg.addEventListener('click', ((counter, id) => {
+    //     return () => {
+    //         document.getElementById(id).innerHTML = ++counter
+    //     }
+    // })(counter, cat))
 }
+
+// for (const cat in cats) {
+//     catImg = document.querySelector('.cat-img')
+//     let counter = 0
+//     catImg.addEventListener('click', ((counter, id) => {
+//         document.getElementById(id).innerHTML = ++counter
+//     })(counter, cat))
+// }
+
